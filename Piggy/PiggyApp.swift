d7a381dde61b8fce13d05ae4062483c59e7519ff
Gustavo Goldhardt <ggoldhardt@microsoft.com>
2024-10-07 10:23:46 -0300

    Initial Commit

---
+++ Piggy/PiggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyApp.swift
+//  Piggy
+//
+//  Created by Gustavo Goldhardt on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PiggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiggyTests/PiggyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyTests.swift
+//  PiggyTests
+//
+//  Created by Gustavo Goldhardt on 07/10/24.
+//
+
+import Testing
+@testable import Piggy
+
+struct PiggyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
